dfad9bd828a16599e0da134a82069ac93f84a901
Tim Beck <becktimd@gmail.com>
2023-02-17 15:44:18 -0800

    stdio wireup

--- app/main.go
+++ app/main.go
@@ -1,27 +1,19 @@
 package main
 
 import (
-	"fmt"
 	"os"
 	"os/exec"
 )
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
-	// Uncomment this block to pass the first stage!
-
 	command := os.Args[3]
 	args := os.Args[4:len(os.Args)]
+	// fmt.Println(command, args)
 
 	cmd := exec.Command(command, args...)
-	output, err := cmd.Output()
-	if err != nil {
-		fmt.Printf("Err: %v", err)
-		os.Exit(1)
-	}
-
-	fmt.Println(string(output))
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Run()
 }
